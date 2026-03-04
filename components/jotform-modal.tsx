91ce1141c437c3ed0b02f6bf227b71a5cc52667f
sriharshamittapalli <harsha.mittapalli@outlook.com>
2026-03-04 10:55:13 -0600

    "Label Change"

--- components/jotform-modal.tsx
+++ components/jotform-modal.tsx
@@ -17,13 +17,13 @@ export function JotFormModal({ open, onOpenChange }: JotFormModalProps) {
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="flex h-[95vh] w-[95vw] max-w-none flex-col p-0 sm:max-w-none">
         <DialogHeader className="px-6 pt-6 pb-2">
-          <DialogTitle>Marketing Assets Form</DialogTitle>
+          <DialogTitle>Evergreen Order Form</DialogTitle>
         </DialogHeader>
         <iframe
           src={process.env.NEXT_PUBLIC_JOTFORM_URL}
           className="flex-1 w-full border-0"
           allow="fullscreen"
-          title="Marketing Assets Form"
+          title="Evergreen Order Form"
         />
       </DialogContent>
     </Dialog>
